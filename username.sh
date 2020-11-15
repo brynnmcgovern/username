566ae1776dfa58a639e5fdac2668358f3afaf775
Brynn McGovern <bmcgovern@chapman.edu>
2020-11-14 17:31:19 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -7,10 +7,15 @@ echo "It must start with a lower case character"
 echo "It must contain at least 3 but not more than 12 characters"
 
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z0-9\*_]{3,12}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9\*_]{3,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username. Please try again."
 	echo "Enter a username: "
 	read USERNAME
+	
+	
+
+
+
 done
 echo "Thank you"
